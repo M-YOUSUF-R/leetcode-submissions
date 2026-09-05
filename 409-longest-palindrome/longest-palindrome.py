ae3171c1bc4013c83e27e8d7d0e5508b94da1405
Md Yousuf <yousufmazumder12@gmail.com>
2026-09-05 18:35:29 +0600

    Time: 3 ms (46.60%) | Memory: 19.3 MB (67.39%) - LeetSync

--- 409-longest-palindrome/longest-palindrome.py
+++ 409-longest-palindrome/longest-palindrome.py
@@ -4,10 +4,10 @@ class Solution:
         length = 0
         for char in s:
             if char not in char_set:
-                char_set.add(char)
+                char_set.add(char) 
             else:
-                length += 2
-                char_set.remove(char)
-        if char_set:
+                length += 2 # we've got a pair , so it can increase the palindrome length
+                char_set.remove(char) # let's remove them so new pair can found
+        if char_set: # there is left only 1 time comming values , so it can increase the length only for once
             length += 1
         return length
