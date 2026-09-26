b839d2d209f9262ec4368337a7903be09fe7a43d
Md Yousuf <yousufmazumder12@gmail.com>
2026-09-26 11:37:26 +0600

    Time: 180 ms (41.23%) | Memory: 40.6 MB (59.21%) - LeetSync

--- 148-sort-list/sort-list.py
+++ 148-sort-list/sort-list.py
@@ -3,19 +3,33 @@
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
-class Solution:
-    def sortList(self, head: ListNode | None) -> ListNode | None:
-        if not head:
-            return None
-        nodes = []
-        current = head
-        while current:
-            nodes.append(current.val)
-            current = current.next
-        nodes.sort()
-        head = ListNode(nodes[0])
-        current = head
-        for item in nodes[1:]:
-            current.next = ListNode(item)
-            current = current.next
-        return head
+
+class Solution(object):
+    def sortList(self, head):
+        if not head or not head.next:
+            return head
+        slow, fast = head, head
+        prev = None
+        while fast and fast.next:
+            prev = slow
+            slow = slow.next
+            fast = fast.next.next
+        prev.next = None
+        left = self.sortList(head)
+        right = self.sortList(slow)
+        return self.merge(left, right)
+
+    def merge(self, l1, l2):
+        dummy = ListNode(0)
+        cur = dummy
+        while l1 and l2:
+            if l1.val < l2.val:
+                cur.next = l1
+                l1 = l1.next
+            else:
+                cur.next = l2
+                l2 = l2.next
+            cur = cur.next
+        if l1: cur.next = l1
+        if l2: cur.next = l2
+        return dummy.next
